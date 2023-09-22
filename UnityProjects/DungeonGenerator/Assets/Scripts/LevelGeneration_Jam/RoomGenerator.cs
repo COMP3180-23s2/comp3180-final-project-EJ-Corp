38db44863ca455c5e7814f417def2a3a8cbaa8ee
EJ-Corp <emgdl2002@gmail.com>
2023-09-22 15:38:29 +1000

    Initial Creation for 2 Algorithms

---
+++ UnityProjects/DungeonGenerator/Assets/Scripts/LevelGeneration_Jam/RoomGenerator.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomGenerator : MonoBehaviour
+{
+    [SerializeField] private RoomTemplates templates;
+    [SerializeField] private int roomRoll;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        templates = GameObject.FindGameObjectWithTag("Templates").GetComponent<RoomTemplates>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Spawn()
+    {
+        roomRoll = Random.Range(0, templates.allRooms.Length);
+        GameObject spawnedRoom = Instantiate(templates.allRooms[roomRoll]);
+        spawnedRoom.transform.position = transform.position;
+    }
+}
